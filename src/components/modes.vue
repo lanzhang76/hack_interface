2b020af2606e3954c335a11be29c88d7fb57bed4
Lan Zhang <zhany016@newschool.edu>
2020-02-26 15:23:29 -0500

    first-commit

---
+++ src/components/modes.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>Current Mode: {{ picked }}</span>
+    <br />
+    <input type="radio" id="one" value="Write" v-model="picked" />
+    <label for="one">Write</label>
+    <br />
+    <input type="radio" id="two" value="Group Improv" v-model="picked" />
+    <label for="two">Group Improv</label>
+    <br />
+    <input type="radio" id="three" value="Rant" v-model="picked" />
+    <label for="two">Rant</label>
+    <br />
+  </div>
+</template>>
+
+<script>
+export default {
+  name: "modes",
+  data() {
+    return {
+      picked: ""
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
